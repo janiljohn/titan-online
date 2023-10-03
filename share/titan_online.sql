578cdf9d84cb728627289777afbceed9a168ca6e
PMarcoG10 <marcog10@csu.fullerton.edu>
2023-10-02 17:55:58 -0700

    Updated the sql file

--- share/titan_online.sql
+++ share/titan_online.sql
@@ -14,3 +14,40 @@ CREATE TABLE Class (
 
 INSERT INTO Class(classID, department, sectionNum, name, maxEnrollement, currentEnrollment, professorID) VALUES(1, 'Computer Science', 2, 'test',3, 4, 5);
 COMMIT;
+
+DROP TABLE IF EXISTS Enrollment;
+CREATE TABLE Enrollment (
+    enrollmentID INT PRIMARY KEY,
+    CWID INT,
+    classID INT,
+    enrollmentDate DATE,
+    dropped BOOLEAN
+    -- FOREIGN KEY (CWID) REFERENCES Student(CWID),
+    -- FOREIGN KEY (classID) REFERENCES Class(classID)
+);
+
+DROP TABLE IF EXISTS Professor;
+CREATE TABLE Professor (
+    professorID INT PRIMARY KEY,
+    firstName TEXT,
+    lastName TEXT,
+    email TEXT
+);
+
+DROP TABLE IF EXISTS Student;
+CREATE TABLE Student (
+    CWID INT PRIMARY KEY,
+    firstName TEXT,
+    lastName TEXT,
+    email TEXT
+);
+
+DROP TABLE IF EXISTS WaitingList;
+CREATE TABLE WaitingList (
+    waitingID INT PRIMARY KEY,
+    CWID INT,
+    classID INT,
+    position INT
+    -- FOREIGN KEY (CWID) REFERENCES Student(CWID),
+    -- FOREIGN KEY (classID) REFERENCES Class(classID)
+);
